a2b361731c9f1bb57d9d7ca347ddf654c0af3b7c
Manasa Rao <drmanasa18.rao@gmail.com>
2020-02-11 15:57:07 +0100

    adds files

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Destination from './components/Destination'
-import EmbedMap from "./components/EmbedMap"
+import Destination from './components/Destination';
+import EmbedMap from "./components/EmbedMap";
 import Login from "./components/Login";
-import Trip from './components/Trip.jsx'
+import Trip from './components/Trip.jsx';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 const App = () => {
@@ -10,11 +10,10 @@ const App = () => {
       <div className="ui container">
         <BrowserRouter>
           <div className="App">
-          <Login />
             <Switch>
               <Route exact path="/" component={Destination}></Route>
               <Route exact path="/trip" component={Trip}></Route>
-            </Switch>
+              </Switch>
           </div>
         </BrowserRouter>
       </div>
